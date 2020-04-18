7a7d5855170620da257c66ace865d9ec617308f6
Ryan Lewin <lewin.ry+development@gmail.com>
2020-04-18 11:03:34 +1000

    Initial commit

---
+++ FavoriteThingsTests/FavoriteThingsTests.swift
@@ -0,0 +1,34 @@
+//
+//  FavoriteThingsTests.swift
+//  FavoriteThingsTests
+//
+//  Created by Ryan Lewin on 18/4/20.
+//  Copyright © 2020 Ryan Lewin. All rights reserved.
+//
+
+import XCTest
+@testable import FavoriteThings
+
+class FavoriteThingsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
