fd9b0db7244be5a062eb3b11e242e211db3b2b15
Ryan Lewin <lewin.ry+development@gmail.com>
2020-04-25 15:42:04 +1000

    Adds core data model

---
+++ Item_info+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Item_info+CoreDataClass.swift
+//  FavoriteThings
+//
+//  Created by Ryan Lewin on 25/4/20.
+//  Copyright © 2020 Ryan Lewin. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Item_info)
+public class Item_info: NSManagedObject {
+
+}
